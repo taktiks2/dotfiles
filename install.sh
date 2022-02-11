acc9c84c715c8010d4ee52662fd0bd06eec3f682
taktiks2 <deathproof.lee@gmail.com>
2022-02-11 10:32:04 +0900

    Add color on terminal mode

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
+# vimの設定ファイルを$HOMEにシンボリックリンクとして追加
 cd ~
 ln -s ~/dotfiles/.vim .
 ln -s ~/dotfiles/.vimrc .
 
+# Fernにアイコンを表示するためのフォントを配置
+# Debianでは動作確認済 Archでは上手く行かなかった
 mkdir -p ~/.local/share/fonts
 cd .local/share/
 ln -s ~/dotfiles/fonts/ .
 
+# Braceyのためのvim-plugを導入するためのコマンド
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
