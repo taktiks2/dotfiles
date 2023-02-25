9681d0fd257611a7184dc425b987ac59e004841c
taktiks2 <deathproof.lee@gmail.com>
2023-02-25 14:50:12 +0900

    update

--- install.sh
+++ install.sh
@@ -1,32 +1,9 @@
 #!/bin/bash
-
-# vimの設定ファイルを$HOMEにシンボリックリンクとして追加
-ln -s ~/dotfiles/.vim ~/
-ln -s ~/dotfiles/.vimrc ~/
-ln -s ~/dotfiles/.gitconfig ~/
-ln -s ~/dotfiles/.sqliterc ~/
-
-# conda仮想環境自動推移　ymlファイルがあるところで勝手に推移する
-# conda env export > environment.ymlを実行してymlファイルを作成すること
-# echo "source ~/dotfiles/conda-auto-env/conda_auto_env.sh" >> ~/.bashrc
-
 # tmuxの設定
 mkdir -p ~/.tmux/plugins/tpm
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ln -s ~/dotfiles/.tmux.conf ~/
 tmux source ~/.tmux.conf
 
-# Fernにアイコンを表示するためのフォントを配置
-# Debianでは動作確認済 Archでは上手く行かなかった
-# mkdir -p ~/.local/share/fonts
-# cd .local/share/
-# ln -s ~/dotfiles/fonts/ ~/
-
-# Braceyのためのvim-plugを導入するためのコマンド
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-# ddc.vimのためにdenoをインストールする
-sudo apt install unzip
-curl -fsSL https://deno.land/install.sh | sh
-sudo ln -s ~/.deno/bin/deno /usr/bin/
-set -o noclobber
+# neovim
+ln -s ~/dotfiles/nvim ~/.config/
--- nvim/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo apt remove neovim -y
-sudo add-apt-repository ppa:neovim-ppa/stable
-sudo apt-get update
-sudo apt-get install neovim
-
-mkdir -p ~/.config/nvim
-ln -s ~/dotfiles/nvim ~/.config
