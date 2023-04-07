e27919588b8524ea0dd2e9538ae648f5f0d4f149
taktiks2 <deathproof.lee@gmail.com>
2023-04-07 22:28:44 +0900

    update

--- .config/nvim/after/plugin/telescope.rc.lua
+++ .config/nvim/after/plugin/telescope.rc.lua
@@ -20,7 +20,7 @@ telescope.setup({
 
 telescope.load_extension("file_browser")
 
-vim.keymap.set('n', ';f', builtin.find_files, {})
+vim.keymap.set('n', ';f', ':Telescope find_files hidden=true<CR>', {})
 vim.keymap.set('n', ';g', builtin.live_grep, {})
 vim.keymap.set('n', ';b', builtin.buffers, {})
 vim.keymap.set('n', ';h', builtin.help_tags, {})
