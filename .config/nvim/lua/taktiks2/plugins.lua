d043271b37465ca54e0731a497167f5a7110dfbe
taktiks2 <deathproof.lee@gmail.com>
2023-07-12 00:16:57 +0900

    add leap and some other plugins

--- .config/nvim/lua/taktiks2/plugins.lua
+++ .config/nvim/lua/taktiks2/plugins.lua
@@ -43,22 +43,29 @@ packer.startup(function(use)
 
   use 'lewis6991/gitsigns.nvim'
   use 'dinhhuy258/git.nvim'
+  use {
+    'f-person/git-blame.nvim',
+    config = function() vim.g.gitblame_enabled = 0 end,
+  }
+
+  use 'yamatsum/nvim-cursorline'
+
+  use {
+    'ggandor/leap.nvim',
+    config = function() require('leap').add_default_mappings() end,
+  }
 
   use {
     "akinsho/toggleterm.nvim",
     tag = '*',
-    config = function()
-    end
   }
-
-  use 'TimUntersberger/neogit'
 
   use {
     'nvim-treesitter/nvim-treesitter',
     run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
   }
-
   use 'virchau13/tree-sitter-astro'
+  use 'HiPhish/nvim-ts-rainbow2'
 
   use({
     "iamcco/markdown-preview.nvim",
