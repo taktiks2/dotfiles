034e9e0c87cbfda77e8e6f5beda0f767d5836e7a
taktiks2 <deathproof.lee@gmail.com>
2023-11-19 20:14:28 +0900

    update

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -39,15 +39,6 @@ return {
         hide = {
           cursorline = true,
         },
-        -- render = function(props)
-        --   local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
-        --   if vim.bo[props.buf].modified then
-        --     filename = "[+] " .. filename
-        --   end
-        --
-        --   local icon, color = require("nvim-web-devicons").get_icon_color(filename)
-        --   return { { icon, guifg = color }, { " " }, { filename } }
-        -- end,
         render = function(props)
           local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
           if vim.bo[props.buf].modified then
