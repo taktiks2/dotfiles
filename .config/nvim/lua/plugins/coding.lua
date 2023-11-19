14dfab7b8b794107be08f99bac87b92ddb5c830a
taktiks2 <deathproof.lee@gmail.com>
2023-11-19 20:14:28 +0900

    update

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -1,7 +1,23 @@
 return {
-  "Wansmer/treesj",
-  dependencies = { "nvim-treesitter/nvim-treesitter" },
-  config = function()
-    require("treesj").setup({})
-  end,
+  {
+    "Wansmer/treesj",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    config = function()
+      require("treesj").setup({})
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      local cmp = require("cmp")
+
+      opts.preselect = cmp.PreselectMode.None
+      opts.completion = {
+        completeopt = "menu,menuone,noinsert,noselect",
+      }
+      opts.mapping = cmp.mapping.preset.insert(vim.tbl_extend("force", opts.mapping, {
+        ["<CR>"] = cmp.mapping.confirm({ select = false }), -- 'select = false' to only confirm explicitly selected item
+      }))
+    end,
+  },
 }
