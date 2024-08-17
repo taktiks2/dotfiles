794f240268478e3decba78f5e99c620b6b157da0
taktiks2 <deathproof.lee@gmail.com>
2024-08-17 21:56:48 +0900

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,6 +8,7 @@ set -g theme_nerd_fonts yes
 set -g theme_newline_cursor yes
 set -g theme_newline_prompt '> '
 
+set -x VIRTUAL_ENV_DISABLE_PROMPT 1
 set -x LANG en_US.UTF-8
 set PATH /opt/homebrew/bin $PATH
 set PATH $HOME/Library/Android/sdk/cmdline-tools $PATH
@@ -21,23 +22,28 @@ set PATH /opt/homebrew/opt/mysql@8.0/bin $PATH
 set DYLD_LIBRARY_PATH /opt/homebrew/opt/mysql@8.0/lib $DYLD_LIBRARY_PATH
 set -x ANDROID_SDK_ROOT $HOME/Library/Android/sdk
 set -x JAVA_HOME /Library/Java/JavaVirtualMachines/jdk-17.jdk/Contents/Home
+
+# nodebrew
+set -x PATH $HOME/.nodebrew/current/bin $PATH
+
 # rust tools
 set PATH $HOME/.cargo/bin $PATH
 
-# set -gx PATH '/Users/taktiks2/.rbenv/shims' $PATH
-# set -gx RBENV_SHELL fish
-# command rbenv rehash 2>/dev/null
-# function rbenv
-#   set command $argv[1]
-#   set -e argv[1]
-#
-#   switch "$command"
-#   case rehash shell
-#     rbenv "sh-$command" $argv|source
-#   case '*'
-#     command rbenv "$command" $argv
-#   end
-# end
+# rbenv
+set -gx PATH '/Users/taktiks2/.rbenv/shims' $PATH
+set -gx RBENV_SHELL fish
+command rbenv rehash 2>/dev/null
+function rbenv
+  set command $argv[1]
+  set -e argv[1]
+
+  switch "$command"
+  case rehash shell
+    rbenv "sh-$command" $argv|source
+  case '*'
+    command rbenv "$command" $argv
+  end
+end
 
 alias vim="nvim"
 alias ls="lsd"
